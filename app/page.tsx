1fc53f355e5132b8ae55171a11fae5d9e347dbca
frbarbre <fr.barbre@gmail.com>
2023-07-27 10:55:50 +0200

    maybe :)

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,6 @@
 //   );
 // }
 
-
 import { ProjectInterface } from "@/common.types";
 import Categories from "@/components/Categories";
 import LoadMore from "@/components/LoadMore";
@@ -84,11 +83,11 @@ import { fetchAllProjects } from "@/lib/actions";
 type SearchParams = {
   category?: string | null;
   endcursor?: string | null;
-}
+};
 
 type Props = {
-  searchParams: SearchParams
-}
+  searchParams: SearchParams;
+};
 
 type ProjectSearch = {
   projectSearch: {
@@ -99,15 +98,19 @@ type ProjectSearch = {
       startCursor: string;
       endCursor: string;
     };
-  },
-}
+  };
+};
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 export const dynamicParams = true;
 export const revalidate = 0;
 
 const Home = async ({ searchParams: { category, endcursor } }: Props) => {
-  const data = await fetchAllProjects(category, endcursor) as ProjectSearch
+  let data = (await fetchAllProjects(endcursor)) as ProjectSearch;
+
+  if (category !== "") {
+    data = (await fetchAllProjects(category, endcursor)) as ProjectSearch;
+  }
 
   const projectsToDisplay = data?.projectSearch?.edges || [];
 
@@ -116,9 +119,11 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
       <section className="flexStart flex-col paddings">
         <Categories />
 
-        <p className="no-result-text text-center">No projects found, go create some first.</p>
+        <p className="no-result-text text-center">
+          No projects found, go create some first.
+        </p>
       </section>
-    )
+    );
   }
 
   return (
@@ -139,14 +144,14 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
         ))}
       </section>
 
-      <LoadMore 
-        startCursor={data?.projectSearch?.pageInfo?.startCursor} 
-        endCursor={data?.projectSearch?.pageInfo?.endCursor} 
-        hasPreviousPage={data?.projectSearch?.pageInfo?.hasPreviousPage} 
+      <LoadMore
+        startCursor={data?.projectSearch?.pageInfo?.startCursor}
+        endCursor={data?.projectSearch?.pageInfo?.endCursor}
+        hasPreviousPage={data?.projectSearch?.pageInfo?.hasPreviousPage}
         hasNextPage={data?.projectSearch?.pageInfo?.hasNextPage}
       />
     </section>
-  )
+  );
 };
 
 export default Home;
